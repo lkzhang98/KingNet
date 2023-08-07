2b42b5083af51374d8a7ff58bc6430187643d9ea
lkzhang17 <lkzhang98@163.com>
2023-08-08 00:21:58 +0800

    commit origin project code and add todo list

--- cmd/kingnet/main.go
+++ cmd/kingnet/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"go.mod/internal/kingnet"
+	"os"
+)
+
+func main() {
+	command := kingnet.NewKingNetCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
--- go.mod
+++
@@ -1,8 +0,0 @@
-module KingNet
-
-go 1.16
-
-require (
-	github.com/pkg/errors v0.9.1
-	go.uber.org/zap v1.25.0
-)
